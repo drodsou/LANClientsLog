bb188a0668bc1bf85de72ccb198ecea0959cbdee
David R. Souto <drodsou@gmail.com>
2013-09-26 09:41:44 +0200

    Create equipos_ping_log.sql

---
+++ util/equipos_ping_log.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `equipos_ping_log` (
+  `ip` varchar(15) DEFAULT NULL,
+  `host` varchar(50) DEFAULT NULL,
+  `mac` varchar(45) DEFAULT NULL,
+  `marca` varchar(45) DEFAULT NULL,
+  `fecha` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Log de pings a maquinas de la red'$$
